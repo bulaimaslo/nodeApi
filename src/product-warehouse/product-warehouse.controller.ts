7d55a9b002cac1340c19ffbaa219852def55b34e
Adam Neumann <57113336+bulaimaslo@users.noreply.github.com>
2022-11-21 00:56:54 +0100

    Added products controller

---
+++ src/product-warehouse/product-warehouse.controller.ts
@@ -0,0 +1,36 @@
+import { Controller, Delete, Get, Patch, Post } from '@nestjs/common';
+import { ProductWarehouseService } from './product-warehouse.service';
+import { Product } from '../product/product';
+import { ProductWarehouse } from './product-warehouse';
+
+@Controller('product-warehouse')
+export class ProductWarehouseController {
+  constructor(
+    private readonly productWarehouseService: ProductWarehouseService,
+  ) {}
+
+  @Get()
+  async findAll(): Promise<Product[]> {
+    return this.productWarehouseService.findAll();
+  }
+
+  @Get(':id')
+  async findOne(id: number): Promise<Product> {
+    return this.productWarehouseService.findOne(id);
+  }
+
+  @Patch(':id')
+  async update(id: number, product: Product): Promise<Product> {
+    return this.productWarehouseService.update(id, product);
+  }
+
+  @Post(':id')
+  async create(product: Product): Promise<Product> {
+    return this.productWarehouseService.create(product);
+  }
+
+  @Delete(':id')
+  async delete(id: number): Promise<Product> {
+    return this.productWarehouseService.delete(id);
+  }
+}
--- src/product-warehouse/product-warehouse.module.ts
+++ src/product-warehouse/product-warehouse.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ProductWarehouseService } from './product-warehouse.service';
+import { ProductWarehouseController } from './product-warehouse.controller';
 
 @Module({
-  providers: [ProductWarehouseService]
+  providers: [ProductWarehouseService],
+  controllers: [ProductWarehouseController]
 })
 export class ProductWarehouseModule {}
